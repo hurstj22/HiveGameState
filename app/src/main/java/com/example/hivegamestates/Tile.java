dc590f69d4743583c4646335d8b083c42a81515c
alishee <sheehan24@up.edu>
2021-10-03 14:50:47 -0700

    Update Tile class
    Added enum Piece
    added basic getters / setters
    added empty and normal constructor

--- app/src/main/java/com/example/hivegamestates/Tile.java
+++ app/src/main/java/com/example/hivegamestates/Tile.java
@@ -7,6 +7,50 @@ import android.graphics.Path;
 public class Tile {
 
 
+    public enum Piece {
+        EMPTY,
+        POTENTIAL,
+        QUEEN_BEE,
+        BEETLE,
+        ANT,
+        GRASSHOPPER,
+        SPIDER
+    }
+
+    // Class Variables
+    private Piece type;
+    private Tile onTopOf;
+//    private int coordX;  Do we need these in here? This would represent the position in the list
+//    private int coordY;  to make it (potentially) easier to check for valid moves.
+
+
+    public Tile() {
+        //default constructor could make a null tile
+        type = Piece.EMPTY;
+        onTopOf = null;
+    }
+
+    public Tile(Piece piece, Tile tile) {
+        type = piece;
+        onTopOf = tile;
+    }
+
+    public void setType(Piece piece) {
+        type = piece;
+    }
+
+    public void setOnTopOf(Tile tile) {
+        onTopOf = tile;
+    }
+
+    public Piece getType() {
+        return type;
+    }
+
+    public Tile getOnTopOf() {
+        return onTopOf;
+    }
+
     /**
      * Method for drawing hexagon.
      * @param mCanvas
