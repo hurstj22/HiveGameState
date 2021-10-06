30d397bfedaed6c2fa1e9eb4fb97b212f3d0a625
Kelly Nguyen <nguyenke22@up.edu>
2021-10-06 11:10:37 -0700

    set up test button & editext button, started working on the on click method

--- app/src/main/java/com/example/hivegamestates/MainActivity.java
+++ app/src/main/java/com/example/hivegamestates/MainActivity.java
@@ -8,14 +8,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText output = findViewById(R.id.editText);
-    Button runTest = findViewById(R.id.runTestButton);
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText output = findViewById(R.id.textedit);
+    Button runTest = findViewById(R.id.running);
+    //global declarations
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        runTest.setOnClickListener((View.OnClickListener)this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        output.setText("", TextView.BufferType.EDITABLE);
+
+
+        output.setText("", TextView.BufferType.EDITABLE);
+        //first instantance goes where qutations go
+
+        output.setText("", TextView.BufferType.EDITABLE);
+        //second instantance goes where qutations go
 
 
         runTest.setOnClickListener((View.OnClickListener)this);
