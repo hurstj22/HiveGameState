58e30e6eab685df076fdac42b06557dcca0e4169
yamesboy <89492659+hurstj22@users.noreply.github.com>
2021-10-07 19:28:45 -0700

    implemented editText layouts

--- app/src/main/java/com/example/hivegamestates/MainActivity.java
+++ app/src/main/java/com/example/hivegamestates/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText output = findViewById(R.id.editText);
+    Button runTest = findViewById(R.id.runTestButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         runTest.setOnClickListener((View.OnClickListener)this);
