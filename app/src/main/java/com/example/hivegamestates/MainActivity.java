5af9c0634181c0bbeddd3dd5aad54da1a979eff9
Kelly Nguyen <nguyenke22@up.edu>
2021-10-07 19:20:36 -0700

    set up test button & editext button, started working on the on click method

--- app/src/main/java/com/example/hivegamestates/MainActivity.java
+++ app/src/main/java/com/example/hivegamestates/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hivegamestates;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        runTest.setOnClickListener((View.OnClickListener)this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        output.setText("", TextView.BufferType.EDITABLE);
+
+
+        output.setText("", TextView.BufferType.EDITABLE);
+        //first instance goes where qutations go
+
+        output.setText("", TextView.BufferType.EDITABLE);
+        //second instance goes where qutations go
     }
 }
