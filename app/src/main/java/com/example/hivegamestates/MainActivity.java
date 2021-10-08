60d1f2de2f4a3886e448cee422f5fe0f530b15aa
yamesboy <89492659+hurstj22@users.noreply.github.com>
2021-10-07 20:06:57 -0700

    making quick changes

--- app/src/main/java/com/example/hivegamestates/MainActivity.java
+++ app/src/main/java/com/example/hivegamestates/MainActivity.java
@@ -11,40 +11,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText output = findViewById(R.id.editText);
     Button runTest = findViewById(R.id.runTestButton);
+    //making firstInstance of gameState
+    HiveGameState firstInstance = new HiveGameState();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //making firstInstance of gameState
-        HiveGameState firstInstance = new HiveGameState();
-        //to help us adding some starting tiles... I defintely made that more complicated than it had to be
-        for (int i = 2; i < 4; i++) {
-            for (int j = 3; j < 6; j++) {
-                if (i == 2) {
-                    Tile blackTile = new Tile(i, j, Tile.PlayerPiece.BLACK);
-                    if (j == 0) {
-                        blackTile.setType(Tile.Bug.BEETLE);
-                    } else if (j == 1) {
-                        blackTile.setType(Tile.Bug.GRASSHOPPER);
-                    } else {
-                        blackTile.setType(Tile.Bug.QUEEN_BEE);
-                    }
-                    firstInstance.addTile(blackTile);
-                } else {
-                    Tile whiteTile = new Tile(i, j, Tile.PlayerPiece.WHITE);
-                    if (j == 0) {
-                        whiteTile.setType(Tile.Bug.BEETLE);
-                    } else if (j == 1) {
-                        whiteTile.setType(Tile.Bug.GRASSHOPPER);
-                    } else {
-                        whiteTile.setType(Tile.Bug.QUEEN_BEE);
-                    }
-                    firstInstance.addTile(whiteTile);
-                }
-            }
-        }
 
         runTest.setOnClickListener((View.OnClickListener) this);
 
