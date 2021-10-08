f3b283cdf280c9c1f1155a32feb9f6e80038205e
yamesboy <89492659+hurstj22@users.noreply.github.com>
2021-10-07 20:09:32 -0700

    implemented the first instances

--- app/src/main/java/com/example/hivegamestates/MainActivity.java
+++ app/src/main/java/com/example/hivegamestates/MainActivity.java
@@ -18,16 +18,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         runTest.setOnClickListener((View.OnClickListener) this);
 
+        setFirstInstance(); //creates the first test case
     }
 
     @Override
     public void onClick(View view) {
 
-        output.setText("", TextView.BufferType.EDITABLE);
-
+        output.setText(firstInstance.toString(), TextView.BufferType.EDITABLE);
 
         output.setText("", TextView.BufferType.EDITABLE);
         //first instance goes where qutations go
@@ -36,4 +35,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //second instance goes where qutations go
 
     }
+
+    public void setFirstInstance(){
+
+        //to help us adding some starting tiles... I defintely made that more complicated than it had to be
+        for (int i = 2; i < 4; i++) {
+            for (int j = 3; j < 6; j++) {
+                if (i == 2) {
+                    Tile blackTile = new Tile(i, j, Tile.PlayerPiece.BLACK);
+                    if (j == 0) {
+                        blackTile.setType(Tile.Bug.BEETLE);
+                    } else if (j == 1) {
+                        blackTile.setType(Tile.Bug.GRASSHOPPER);
+                    } else {
+                        blackTile.setType(Tile.Bug.QUEEN_BEE);
+                    }
+                    firstInstance.addTile(blackTile);
+                } else {
+                    Tile whiteTile = new Tile(i, j, Tile.PlayerPiece.WHITE);
+                    if (j == 0) {
+                        whiteTile.setType(Tile.Bug.BEETLE);
+                    } else if (j == 1) {
+                        whiteTile.setType(Tile.Bug.GRASSHOPPER);
+                    } else {
+                        whiteTile.setType(Tile.Bug.QUEEN_BEE);
+                    }
+                    firstInstance.addTile(whiteTile);
+                }
+            }
+        }
+    }
+
 }
