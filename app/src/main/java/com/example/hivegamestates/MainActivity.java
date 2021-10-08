94306b7330a8462cfc05f10e575d936b93573675
Isaac Reinhard <33108662+ireinhard@users.noreply.github.com>
2021-10-07 19:45:27 -0700

    Added tiles to board

--- app/src/main/java/com/example/hivegamestates/MainActivity.java
+++ app/src/main/java/com/example/hivegamestates/MainActivity.java
@@ -10,5 +10,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //making firstInstance of gameState
+        HiveGameState firstInstance = new HiveGameState();
+        //to help us adding some starting tiles... I defintely made that more complicated than it had to be
+        for(int i = 2; i < 4; i++){
+            for(int j = 3; j < 6; j++){
+                if(i == 2){
+                    Tile blackTile = new Tile(i, j, Tile.PlayerPiece.BLACK);
+                    if (j == 0){
+                        blackTile.setType(Tile.Bug.BEETLE);
+                    }
+                    else if (j == 1){
+                        blackTile.setType(Tile.Bug.GRASSHOPPER);
+                    }
+                    else{
+                        blackTile.setType(Tile.Bug.QUEEN_BEE);
+                    }
+                    firstInstance.addTile(blackTile);
+                }
+                else{
+                    Tile whiteTile = new Tile(i, j, Tile.PlayerPiece.WHITE);
+                    if (j == 0){
+                        whiteTile.setType(Tile.Bug.BEETLE);
+                    }
+                    else if (j == 1){
+                        whiteTile.setType(Tile.Bug.GRASSHOPPER);
+                    }
+                    else{
+                        whiteTile.setType(Tile.Bug.QUEEN_BEE);
+                    }
+                    firstInstance.addTile(whiteTile);
+                }
+            }
+        }
+
     }
 }
